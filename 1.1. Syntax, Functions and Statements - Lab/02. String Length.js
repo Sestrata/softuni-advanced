f67ba4ec115edd2bb9993cc741514650fb2f8d88
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2022-04-14 12:53:05 +0300

    Create 02. String Length.js

---
+++ 1.1. Syntax, Functions and Statements - Lab/02. String Length.js
@@ -0,0 +1,7 @@
+function strLength(first, second, third) {
+
+    let total = first.length + second.length + third.length;
+    let avg = Math.floor(total / 3);
+    console.log(total);
+    console.log(avg);
+}
