818c3048f27735059ed79617a1b7740f1ab81e11
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2022-04-17 16:16:08 +0300

    Create 01. Even Position Elements.js

---
+++ 2.1. Arrays and Nested Arrays - Lab/01. Even Position Elements.js
@@ -0,0 +1,11 @@
+function evenPositionEl(arr) {
+
+    let newArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i % 2 == 0) {
+            newArr.push(arr[i]);
+        }
+    }
+    console.log(newArr.join(' '));
+}
