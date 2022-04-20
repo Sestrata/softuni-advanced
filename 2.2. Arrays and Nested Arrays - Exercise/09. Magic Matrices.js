7a42a68dc22471fffb618efa3cc94338d3b65841
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2022-04-20 12:08:57 +0300

    Update 09. Magic Matrices.js

--- 2.2. Arrays and Nested Arrays - Exercise/09. Magic Matrices.js
+++ 2.2. Arrays and Nested Arrays - Exercise/09. Magic Matrices.js
@@ -5,7 +5,7 @@ function magicMatricies(arr) {
 
     for (let i = 0; i < arr.length; i++) {
         let sumRow = arr[i].reduce((a, b) => a + b, 0);
-        let sumCol = arr.map(row => row[i]).reduce((a, b) => a + b, 0); //sum the nums from each row, starting with 0
+        let sumCol = arr.map(row => row[i]).reduce((a, b) => a + b, 0);
 
         if (sumRow !== sum || sumCol !== sum) {
             isMagic = false;
