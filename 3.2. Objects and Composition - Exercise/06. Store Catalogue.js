e74671ce74eb96214327bea7f3c39f979132c2b9
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2022-05-06 14:42:46 +0300

    Create 06. Store Catalogue.js

---
+++ 3.2. Objects and Composition - Exercise/06. Store Catalogue.js
@@ -0,0 +1,29 @@
+function storeCatalogue(input) {
+
+    let listOfProducts = {};
+
+    for (let item of input) {
+        let [product, price] = item.split(' : ');
+        price = Number(price);
+        let initial = product[0].toUpperCase();
+        
+        if (listOfProducts[initial] === undefined) {
+            listOfProducts[initial] = {};
+        }
+        listOfProducts[initial][product] = price;
+    }
+    let sortedProducts = Object.keys(listOfProducts);
+    sortedProducts.sort((a, b) => a.localeCompare(b)); //сортираме ключа /само буквите/
+    let result = [];
+
+    for (let key of sortedProducts) {
+        let products = Object.entries(listOfProducts[key])
+            .sort((a, b) => a[0].localeCompare(b[0])); //сортираме обекта от артикулите във всеки обект
+        result.push(key);
+        let productsAsString = products
+            .map(x => `  ${x[0]}: ${x[1]}`)
+            .join('\n');
+        result.push(productsAsString);
+    }
+    console.log(result.join('\n'))
+}
