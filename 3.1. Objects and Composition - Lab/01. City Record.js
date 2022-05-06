35b7345c321d2a0f713b95bda4fdea68205544b9
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2022-05-06 14:33:39 +0300

    Create 01. City Record.js

---
+++ 3.1. Objects and Composition - Lab/01. City Record.js
@@ -0,0 +1,16 @@
+function cityRecord(name, population, treasury) {
+
+    let resultObj = {
+        name: name,
+        population: population,
+        treasury: treasury
+    };
+
+    //or:
+    // let resultObj = {};
+    // resultObj.name = name;
+    // resultObj.population = population;
+    // resultObj.treasury = treasury;
+    
+    return resultObj;
+}
