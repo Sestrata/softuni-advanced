c6ad6977075b6a5abff0e25336a514d1836ee5af
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2022-05-09 19:37:51 +0300

    Create search.js

---
+++ 4.2. DOM Introduction - Exercise/04. Search in List/search.js
@@ -0,0 +1,19 @@
+function search() {
+
+   let userWord = document.getElementById('searchText').value;
+   let townsEl = Array.from(document.querySelectorAll('#towns li')); // правим го на масив
+   let match = document.getElementById('result');
+   let count = 0;
+
+   for (let town of townsEl) {
+      if (town.textContent.includes(userWord)) {
+         count++;
+         town.style.fontWeight = 'bold';
+         town.style.textDecoration = 'underline';
+      } else {
+         town.style.fontWeight = 'normal';
+         town.style.textDecoration = 'none';
+      }
+   }
+   match.textContent = (`${count} matches found`);
+}
