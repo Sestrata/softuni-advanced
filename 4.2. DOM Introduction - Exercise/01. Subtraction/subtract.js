1bbc8cba89fbfb6c8b899820e906e12191deb26f
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2022-05-09 17:50:09 +0300

    Update subtract.js

--- 4.2. DOM Introduction - Exercise/01. Subtraction/subtract.js
+++ 4.2. DOM Introduction - Exercise/01. Subtraction/subtract.js
@@ -1,1 +1,10 @@
+function subtract() {
 
+    let firstNum = document.getElementById('firstNumber');
+    let firstNumAsNum = Number(firstNum.value);
+    let secondNum = document.getElementById('secondNumber');
+    let secondNumAsNum = Number(secondNum.value);
+    let result = firstNumAsNum - secondNumAsNum;
+    let resultElement = document.getElementById('result');
+    resultElement.textContent = result;
+}
