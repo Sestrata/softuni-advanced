7906aec3147aebc5c07b0227050320a56b88e5f0
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2022-06-12 19:50:02 +0300

    Create symmetric.test.js

---
+++ 7.1. Unit Testing and Error Handling - Lab/05.CheckForSymmetry/symmetric.test.js
@@ -0,0 +1,26 @@
+const { expect } = require('chai');
+const { isSymmetric } = require('./symmetric.js');
+
+describe('Symmetry Checker', () => {
+    it('return true for symmetry array', () => {
+        expect(isSymmetric([1, 2, 2, 1])).to.be.true;
+    });
+    it('returns false for non-symmetry array', () => {
+        expect(isSymmetric([1, 2, 3])).to.be.false;
+    });
+    it('returns false for non-array', () => {
+        expect(isSymmetric(5)).to.be.false;
+    });
+    it('works with symmetric odd-length array', () => {
+        expect(isSymmetric([1, 2, 1])).to.be.true;
+    });
+    it('works with symmetric string array', () => {
+        expect(isSymmetric(['a', 'b', 'b', 'a'])).to.be.true;
+    });
+    it('returns false for string param', () => {
+        expect(isSymmetric('abba')).to.be.false;
+    });
+    it('returns false for type mismatched elements', () => {
+        expect(isSymmetric([1, 2, '1'])).to.be.false;
+    });
+});
